9a558e657149d86e5c89a81a2de508a626b1dedd
jheyson-911 <gompanchiro.911@gmail.com>
2023-01-19 17:18:05 -0500

    creacion de las validaciones para el modelo Animes

--- src/model/animes.model.js
+++ src/model/animes.model.js
@@ -13,18 +13,63 @@ const anime = connection.define(
     },
     nombre: {
       type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      comment: 'Campo donde ira el nombre del anime',
     },
     descripcion: {
       type: DataTypes.STRING,
+      comment: 'Campo para la descripcion del anime (opcional)',
     },
-    estado: {
+    visto: {
       type: DataTypes.STRING,
+      defaultValue: 'NO',
+      comment: 'Campo para controlar si el anime fue visto o no ',
+      validate: {
+        isIn: {
+          args: [['SI', 'NO']],
+          msg: 'Solamente puede insertar SI o NO',
+        },
+      },
+    },
+    favorito: {
+      type: DataTypes.STRING,
+      defaultValue: 'NO',
+      comment: 'Campo para seleccionar el anime como facorito',
+      validate: {
+        isUppercase: {
+          args: true,
+          msg: 'Solamente se aceptan mayusculas',
+        },
+        isIn: {
+          args: [['SI', 'NO']],
+          msg: 'Solamente puede insertar SI o NO',
+        },
+      },
     },
     url_img: {
       type: DataTypes.STRING,
+      comment: 'Campo para almacenar la url de la portada del anime',
+      validate: {
+        isUppercase: {
+          args: true,
+          msg: 'Solamente se aceptan mayusculas',
+        },
+        isUrl: {
+          args: true,
+          msg: 'Ingrese una url valida',
+        },
+      },
     },
     calificacion: {
-      type: DataTypes.STRING,
+      type: DataTypes.FLOAT,
+      campo: 'Campo para controlar la calificion del anime',
+      validate: {
+        isFloat: {
+          args: true,
+          msg: 'Ingrese un numero flotante',
+        },
+      },
     },
   },
   {

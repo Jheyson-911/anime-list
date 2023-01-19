5797345cf5bc84acab166280068df98f4ac8c672
jheyson-911 <gompanchiro.911@gmail.com>
2023-01-19 17:19:13 -0500

    creacion de las validaciones para el modelo Tipo

--- src/model/tipo.model.js
+++ src/model/tipo.model.js
@@ -11,6 +11,14 @@ const tipo = connection.define(
     },
     nombre: {
       type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        isAlpha: {
+          args: true,
+          msg: 'Solamente se acepta palabras',
+        },
+      },
     },
   },
   {

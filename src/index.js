ce03d5ff84bd391ba3a8a3145adc266861a77bf1
jheyson-911 <gompanchiro.911@gmail.com>
2023-01-18 20:18:13 -0500

    Estableciendo la conexion con la BD

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import * as dotenv from 'dotenv';
 import app from './app.js';
+import connection from './db/db.js';
 
 dotenv.config();
 app.set('port', process.env.API_PORT || 3000);
 
-app.listen(app.get('port'), () => {
-  console.log(`Listen on port ${process.env.API_PORT}`);
-});
+async function connect() {
+  try {
+    await connection.authenticate();
+    console.log(`Conexion exitosa`);
+    app.listen(app.get('port'), () => {
+      console.log(`Listen on port ${process.env.API_PORT}`);
+    });
+  } catch (err) {
+    console.log(
+      `Ocurrio un error al conectar con la Base de datos: ${err.message}`
+    );
+  }
+}
+connect();

cf4d575a96a2706d8b82959c1e5a0926b673adf7
jheyson-911 <gompanchiro.911@gmail.com>
2023-01-19 17:19:45 -0500

    creacion de validaciones para el modelo User

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,5 +1,7 @@
 import { DataTypes } from 'sequelize';
 import connection from '../db/db.js';
+import animes from './animes.model.js';
+import pendientes from './pendientes.model.js';
 
 const user = connection.define(
   'User',
@@ -11,12 +13,28 @@ const user = connection.define(
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: true,
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Solamente se acepta email',
+        },
+      },
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: true,
     },
     rol: {
       type: DataTypes.STRING,
+      defaultValue: 'USER',
+      validate: {
+        isIn: {
+          args: [['ADMIN', 'USER']],
+          msg: 'Solamente se pueden insertar ADMIN y USER',
+        },
+      },
     },
   },
   {
@@ -24,4 +42,25 @@ const user = connection.define(
     comment: 'Este modelo depresenta la tabla de usuarios',
   }
 );
+
+// Asociacion con el modelo animes
+
+user.hasOne(animes, {
+  foreignKey: 'user_Id',
+  allowNull: false,
+});
+animes.belongsTo(user, {
+  foreignKey: 'user_Id',
+  allowNull: false,
+});
+// Asociacion con el modelo pendientes
+
+user.hasOne(pendientes, {
+  foreignKey: 'user_Id',
+  allowNull: false,
+});
+pendientes.belongsTo(user, {
+  foreignKey: 'user_Id',
+  allowNull: false,
+});
 export default user;

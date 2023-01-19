4968b0fcaeef8ee7d17ff4b47288af512a2ffde9
jheyson-911 <gompanchiro.911@gmail.com>
2023-01-19 17:20:35 -0500

    realizando la migracion a la BD con los modelos creados

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import * as dotenv from 'dotenv';
 import app from './app.js';
 import connection from './db/db.js';
-
+/* import AnimeModel from './model/animes.model.js';
+import userModel from './model/user.model.js';
+import etiquetaModel from './model/etiquetas.model.js';
+import tipoModel from './model/tipo.model.js';
+import pendienteModel from './model/pendientes.model.js';
+ */
 dotenv.config();
 app.set('port', process.env.API_PORT || 3000);
 
 async function connect() {
   try {
     await connection.authenticate();
+    // await connection.sync({ force: true });
     console.log(`Conexion exitosa`);
     app.listen(app.get('port'), () => {
       console.log(`Listen on port ${process.env.API_PORT}`);

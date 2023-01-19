b6185737bbaf4aaea930374afd1b299cd940ecd7
jheyson-911 <gompanchiro.911@gmail.com>
2023-01-18 20:38:47 -0500

    creacion del modelo Tipo

---
+++ src/model/tipo.model.js
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize';
+import connection from '../db/db.js';
+
+const tipo = connection.define(
+  'Tipo',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    freezeTableName: true,
+    comment: 'Este modelo representa la tabla de tipo',
+  }
+);
+
+export default tipo;

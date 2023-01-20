cfd43acc03e9329959eb000a1a7249d1e6cc0352
jheyson-911 <gompanchiro.911@gmail.com>
2023-01-20 11:57:00 -0500

    importando las rutas de User

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import authRoutes from './route/user.route.js';
 
 const app = express();
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.json({
     message: 'Success',
   });
 });
+
+app.use('/api/v1', authRoutes);
 
 export default app;
